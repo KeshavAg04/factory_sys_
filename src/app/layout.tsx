33ecfcb91f7f5a35d3889c81dab65170f50bd398
Keshav Agarwal <keshavag1906@gmail.com>
2026-05-12 16:19:13 +0530

    fixing mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -173,7 +173,7 @@ export default function RootLayout({
 
           {/* PAGE CONTENT */}
 
-          <main className="flex-1">
+          <main className="flex-1 pt-32 md: pt-0">
 
             {children}
 
